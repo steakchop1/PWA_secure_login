929332a4abeb0922a831f4bd4d9b723b16f078e5
steakchop1 <elijah.cochrane@education.nsw.gov.au>
2025-11-13 14:43:51 +1100

    Login validation

--- app.py
+++ app.py
@@ -9,6 +9,18 @@ app.secret_key = os.urandom(24)
 def login():
     return render_template('login.html')
 
+@app.route('/login_validation', methods=['post'])
+def login_validation():
+    email = request.form.get('email')
+    password = request.form.get('password')
+
+    connection =sqlite3.connect('LoginData.db')
+    cursor = connection.cursor()
+    user = cursor.execute("SELECT * FROM USERS WHERE email=? AND password=?", (email,password)).fetchall()
+    if len(user) > 0:
+        return 'welcome'
+    else:
+        return redirect('/')
 if __name__ == '__main__':
     app.run(debug=True)
 
