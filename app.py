fe6fa0fa6d84b7d6727b026bd9826ac61867dca8
steakchop1 <elijah.cochrane@education.nsw.gov.au>
2025-11-14 12:08:24 +1100

    Adding users to the database

--- app.py
+++ app.py
@@ -30,6 +30,27 @@ def home():
 
     return render_template('home.html', fname=fname, lname=lname, email=email)
 
+@app.route('/add_user', Methods=['POST'])
+def add_user():
+    fname = request.form.get('fname')
+    lname = request.form.get('lname')
+    email = request.form.get('email')
+    password = request.form.get('password')
+
+    connection = sqlite3.connect('LoginData.db')
+    cursor = connection.cursor()
+
+    ans = cursor.execute("select * from USER where email=? AND password =?", (email,password)).fetchall()
+
+    if len(ans) > 0:
+        connection.close()
+        return render_template('login.html')
+    else:
+        cursor.execute("INSERT INTO USERS(fname,lname,email,password)value(?,?,?,?)", (fname,lname,email,password))
+        connection.commit()
+        connection.close()
+        return render_template('login.html')
+
 
 if __name__ == '__main__':
     app.run(debug=True)
